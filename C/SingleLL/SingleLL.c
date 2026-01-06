20abc706ba71413fa45f2e07594f1dbae1c31261
apex737 <apex737@naver.com>
2026-01-06 09:15:07 +0900

    ;

--- C/SingleLL/SingleLL.c
+++ C/SingleLL/SingleLL.c
@@ -1,1 +1,22 @@
 #include "SingleLL.h"
+
+typedef struct {
+    int id;
+    const char* name;
+    const char* phone;
+    struct Node* pNext;
+} Node;
+
+// 더미헤드 vs 포인터
+Node* createNode(int id, const char* name, const char* phone)
+{
+    Node* pNewNode = (Node*)malloc(sizeof(Node));
+    pNewNode->id = id;
+    strcpy_s()
+}
+
+int main()
+{
+
+    return 1;
+}
---
+++ C/SingleLL/SingleLL.h
@@ -0,0 +1,8 @@
+#ifndef SINGLE_LL_H
+#define SINGLE_LL_H
+
+#include <stdlib.h>
+#include <string.h>
+
+
+#endif
