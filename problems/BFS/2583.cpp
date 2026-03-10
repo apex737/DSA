6c319c9fb010a78e6d3dc84b9c87400a3730f3d9
apex737 <apex737@naver.com>
2026-03-11 07:59:04 +0900

    bfs_2583_solved

--- problems/BFS/2583.cpp
+++ problems/BFS/2583.cpp
@@ -2,15 +2,17 @@
 
 using namespace std;
 
-int dr[]={1,0,-1,0};
-int dc[]={0,1,0,-1};
-// F for fence : 시작좌표로 구성
-bool fence[1001][1001]; // F for Fence
-bool seen[1001][1001]; // S for Seen
+// 일반적 bfs와 다르게 이건 x,y 좌표계를 쓰네
+// (노드의 전파 횟수 + 1) 이 곧 넓이니까 루프에서 카운터만 돌려주면 그게 넓이겠네
+// 1. 먼저 fence 받아서 보드에 표시하고
+// 2. 각 직사각형마다 bfs 카운터 돌린다음
+// 3. 벡터에 넣어서 정렬하면 끝?
+int board[101][101];
+int dx[]={1,0,-1,0};
+int dy[]={0,1,0,-1};
 struct Node {
-  int r,c,d;
+  int x,y;
 };
-int cnt;
 int main()
 {
   int M, N, K;
@@ -18,44 +20,49 @@ int main()
   Node start, end;
   while(K--)
   {
-    cin >> start.r >> start.c;
-    cin >> end.r >> end.c;
-    for(int i = start.r; i < end.r; i++)
-      for(int j = start.c; j < end.c; j++)
-        fence[i][j]=1;
+    // 1. 먼저 fence 받아서 보드에 표시
+    cin >> start.x >> start.y;
+    cin >> end.x >> end.y;
+    for(int i = start.x; i < end.x; i++)
+      for(int j = start.y; j < end.y; j++)
+        board[i][j]=-1;
   }
 
-  // BFS Standard Form
+  // 2. 각 직사각형마다 bfs 카운터 돌리기
+  // + 추가로, 개별 직사각형에 대해서도 돌려야함
+  
   queue<Node> q;
-  vector<int> area;
-  // 1. Full Search를 하되
-  for(int i = 0; i < M; i++)
-    for(int j = 0; j < N; j++)
+  vector<int> areas;
+  int areaCnt = 0;
+  // BFS Standard Form & Full Search
+  for(int i=0;i<N;i++)
+    for(int j=0;j<M;j++)
     {
-      // 2. 방문한적도 없고, 장애물도 아닌것만
-      if(seen[i][j]==0 && fence[i][j]==0)
+      if(board[i][j]!=-1 && board[i][j]==0)
       {
-        seen[i][j]=1; // 플래그 소비
-        q.push({i,j,0});
-        while(1)
+        areaCnt++; board[i][j]=1; 
+        q.push({i,j});
+        int area=1;
+        while(!q.empty())
         {
-          // 나가기전에 어떻게 최대 dist를남길것인지??
           Node cur = q.front(); q.pop();
-          for(int i = 0; i < 4; i++)
+          for(int i=0;i<4;i++)
           {
-            int nr = cur.r + dr[i];
-            int nc = cur.c + dc[i];
-            if(nr<0||nr>=N||nc<0||nc>=M) continue;
-            if(seen[nr][nc] || fence[nr][nc]) continue;
-            seen[nr][nc]=1;
-            q.push({nr,nc,cur.d+1});
+            int nx = cur.x+dx[i];
+            int ny = cur.y+dy[i];
+            if(nx<0||nx>=N||ny<0||ny>=M) continue;
+            if(board[nx][ny]!=0) continue;
+            board[nx][ny]=1;
+            q.push({nx,ny});
+            area++;
           }
         }
-        // 하나의 영역이 종료됨 
-        cnt++;
-
-      }
+        areas.push_back(area);
+      } 
     }
+  // 3. 벡터에 넣어서 정렬하면 끝  
+  sort(areas.begin(), areas.end());
+  cout << areaCnt << "\n";
 
-
+  for(int area : areas) cout << area << " ";
 }
