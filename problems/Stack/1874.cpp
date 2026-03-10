e4e5d975d5770006776ce55b4da5cd606b1ad1f6
apex737 <apex737@naver.com>
2026-03-10 14:01:03 +0900

    stack_1874_1

---
+++ problems/Stack/1874.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+// 1-n 오름차순 스택 push
+// 스택에서 값을 꺼내서 수열을 구성하는게 pop
+/* 8
+
+1. 4까지 push -> max 갱신하면서
+2. pop2
+3. 6,8까지 push -> target-max=2만큼 push
+4. pop2
+5. 다음수가 나보다 작은데 내 포지션에 그 수가 없으면 NO 출력 
+
+*/
+int arr[100001];
+vector<char> res;
+int main()
+{
+  cin.tie(0)->sync_with_stdio(0);
+  cin >> n;
+  for(int i = 0; i < n; i++) cin >> arr[i];
+  stack<int> s;
+  int curMax=0;
+  int temp=0;
+  for(int i = 0; i < n; i++)
+  {
+    // 4 3 6 8 7 5 2 1 
+    if(curMax < arr[i]) {
+      while(curMax<arr[i])
+      { 
+        s.push(++curMax); // 중복삽입 방지를 위한 전위형 
+        res.push_back('+');
+      }
+      s.pop(); res.push_back('-');
+    } else if (arr[i]==s.top()) {
+      s.pop(); res.push_back('-');
+    } else {
+      cout << "NO" << "\n";
+      return 0;
+    }
+  }
+  for(char c : res) cout << c << "\n";
+}
