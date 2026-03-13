2f0e5ac59b0a0ce8fa14643d8a6598e2a8ce5c1e
apex737 <apex737@naver.com>
2026-03-13 20:45:22 +0900

    sim15686_solved

--- problems/SIM/15686.cpp
+++ problems/SIM/15686.cpp
@@ -4,7 +4,9 @@ using namespace std;
 /* 전략
 1. 개별 조합을 계산하기 1, 2는 벡터에 저장한다
 2. 각 조합에 대하여 도시 치킨거리를 구한다. 
-3. 모든 조합에 대해 반복하며, minDist 전역변수를 갱신한다.*/
+3. 모든 조합에 대해 반복하며, minDist를 res에 담는다.
+4. res 내에서 minVal을 출력한다.
+*/
 int n,m;
 
 struct Node {
@@ -35,9 +37,7 @@ int main()
   vector<Node> sel;
   dfs(0, chickens, sel);
   int minD = BIG_INT;
-  for(int val : res) {
-    minD = min(minD, val);
-  }
+  for(int val : res) minD = min(minD, val);
   cout << minD;
 }
 
@@ -54,9 +54,7 @@ int getDist(const vector<Node>& sel)
   {
     chickenDist = BIG_INT;
     for(Node chicken : sel)
-    {
       chickenDist = min(chickenDist, getManhattanDist(house, chicken));
-    }
     sum+=chickenDist;
   }
   return sum;
