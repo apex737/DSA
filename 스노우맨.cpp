ddd3e421c37d9423832bbb19e9547108d30ffcf5
apex737 <apex737@naver.com>
2026-08-25 17:22:33 +0900

    화분은 DP로 풀 수 있을듯

--- 스노우맨.cpp
+++ 스노우맨.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <climits>
-#define INF LLONG_MAX // from climits
 using namespace std;
 int board[55][55];
 
 /* 전략
 1. 같은 행에 있는 모든 연속된 1 이상의 수를 노드로 정의
 2. 2가 포함된 노드가 출발행, 3이 포함된 노드가 도착행
-3. 2의 상하에서 막힌 노드를 INF로 정의
-=> 모든 노드는 행 번호를 저장하고, 막혀있음 여부를 판단하기 위해, 노드의 좌측과 우측을 저장한다
-
+3. 두 노드의 [left, right]가 겹치면 수직이동 가능
 4. 다익스트라 알고리즘을 수행
-1) 최단거리 배열 D를 도입하고, 2가 포함된 노드에서 출발
-2) 노드 벡터를 순회하면서 도달가능 여부를 파악한다. 도달 가능하다면 INF에서 최단거리로 업데이트
-3) 포지션 (head) 업데이트하고 반복
+1) D[i]는 st => i 까지 가는 데 필요한 최소 거리
+2) nextDist = max(D[curr], 이번 수직이동 거리)
 */
 
 struct Node
@@ -26,73 +21,99 @@ struct Node
 int main()
 {
     int N, M;
-    int ans = 0;
     cin >> N >> M;
     for (int i = 0; i < N; i++)
         for (int j = 0; j < M; j++)
             cin >> board[i][j];
 
     vector<Node> v;
+    int st = -1, en = -1;
 
-    // 1. 맵을 스캔해서 다익스트라 노드로 매핑한다
+    // 1. 연속적인 1이상의 수를 다익스트라 노드로 정의
     for (int i = 0; i < N; i++)
         for (int j = 0; j < M; j++)
         {
             if (board[i][j])
             {
-                // 일단 넣고 right는 나중에 알아낼거임
+                // 일단 넣고 (left = j); right는 나중에 알아낼거임
+                int nodeIdx = v.size();
                 v.push_back({i, j, 0, 0});
 
-                // 출발지 또는 도착지 노드인지 확인한다
-                if (board[i][j] == 2)
-                    v.back().mark = 2;
-                else if (board[i][j] == 3)
-                    v.back().mark = 3;
-
-                // right를 알아낸다.
                 int tj = j;
                 while (tj < M && board[i][tj])
                 {
-                    // 출발지 또는 도착지 노드인지 확인한다
-                    if (board[i][j] == 2)
+                    // 출발, 도착 노드인지 확인
+                    if (board[i][tj] == 2)
+                    {
                         v.back().mark = 2;
-                    else if (board[i][j] == 3)
+                        st = nodeIdx;
+                    }
+                    else if (board[i][tj] == 3)
+                    {
                         v.back().mark = 3;
-                    tj++; // 0만나면 멈춤
+                        en = nodeIdx;
+                    }
+                    tj++;
                 }
-                j = tj;
+                v.back().right = tj - 1;
+                j = tj - 1;
             }
         }
 
     // 2. 다익스트라 알고리즘을 돌린다
 
-    // 1) 출발행을 찾고, D 배열을 초기화한다.
-    Node st;
+    // D[i] = st에서 i번 노드까지 가는 데 필요한 최소 LIMIT
     int dSize = v.size();
-    vector<int> D(dSize);
-    fill(D.begin(), D.end(), -INF);
-    D[0] = 0;
+    /*  D[i] == -1;               아직 가는 방법도 모름
+        D[i] != -1 && !isMin[i];  가는 방법은 찾았지만 최단거리인지 모름
+                                  거리 구한게, 최단거리인건 아님
+        isMin[i] == 1;            경로가 최단거리임
+    */
+    vector<int> D(dSize, -1);
+    vector<bool> isMin(dSize, 0);
+    D[st] = 0;
 
-    for (auto n : v)
-        if (n.mark == 2)
-            st = n;
-
-    // 2) 매 루프마다 st를 업데이트하면서 한칸씩 이동하면서 도달가능한 모든 노드에 이르는
-    //    최단거리를 D에 기록한다
-    while (dSize--)
+    // 여기에서 다음 타겟노드 갱신
+    // 아직 확정되지 않은 '도달 가능한' 노드 중 D가 가장 작은 노드를 고른다.
+    for (int i = 0; i < dSize; i++)
     {
-        int minDist = 999;
-        for (auto n : v)
+        int curr = -1;
+        for (int j = 0; j < dSize; j++)
         {
-
-            if (n.left >= st.left && n.right <= st.right)
-            {
-            }
+            // 선택 노드가 없으면 일단 첫번째를 curr로 지정
+            // minIdx 탐색
+            if (!isMin[j] && D[j] != -1 && (curr == -1 || D[j] < D[curr]))
+                curr = j;
         }
 
-        // 마지막에 st를 업데이트해서 한칸 전진
+        if (curr == -1)
+            break;
+
+        isMin[curr] = 1;
+
+        // 보석 발판이 curr로 선택됐다 == D[en]이 최소 LIMIT로 확정됐다
+        if (curr == en)
+            break;
+
+        for (int next = 0; next < dSize; next++)
+        {
+            // 내 왼쪽이나 오른쪽에 안겹치게 있으면 점프 못함
+            bool overlap = v[curr].left <= v[next].right && v[next].left <= v[curr].right;
+            // 최단거리 모르는 것들 중에서 도달 가능한 노드 탐색
+            if (!isMin[next] && overlap)
+            {
+                int d = abs(v[curr].row - v[next].row);
+
+                // 일반적인 다익이면 여기에서 거리 누산
+                // 지금까지 최대 게이지 2만 쓰고 왔는데, 다음껀 게이지 3이 든다면 nextDist = 3;
+                int nextDist = max(D[curr], d);
+                // 최단 거리 갱신
+                if (D[next] == -1 || nextDist < D[next])
+                    D[next] = nextDist;
+            }
+        }
     }
 
-    cout << ans << "\n";
+    cout << D[en] << "\n";
     return 0;
 }
