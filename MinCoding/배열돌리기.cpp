2d15cf9fca30344b78b0ee5fd80803af8ce83e8e
apex737 <apex737@naver.com>
2026-09-02 17:53:56 +0900

    범준이

--- MinCoding/배열돌리기.cpp
+++ MinCoding/배열돌리기.cpp
@@ -1,128 +1,132 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#define TIME_LIMIT 60 * 9
+
+int N, M, T;
 using namespace std;
 
-int N, M, K;
-int mn = 999;
-
-struct Operation
-{
-    int r, c, s;
-};
-
-struct Board
-{
-    int b[55][55];
-};
-
-// 배열을 넘기는 방식
-Board board;
-int getVal(const Board &board)
-{
-    int ret = 999;
-    for (int i = 1; i <= N; i++)
-    {
-        int sum = 0;
-        for (int j = 1; j <= M; j++)
-        {
-            sum += board.b[i][j];
-        }
-        ret = min(ret, sum);
-    }
-    return ret;
-}
-
+int mx;
+int dist[40][40];
+vector<int> hotels;
 vector<bool> seen;
-vector<Operation> Ops;
+int airport;
 
-// (r,c)가 중심이고 연산횟수가 s임
-// 연산을 거듭할 수록 시작점을 좌상단 모서리로 이동시킴
-void rotate(Board &board, const Operation &op)
+struct Node
 {
-    int r = op.r - 1;
-    int c = op.c - 1;
-    for (int ss = 0; ss < op.s; ss++, r--, c--)
-    {
-        int width = 2 * ss + 3;
-        // 1. flatten
-        vector<int> tv;
-        // 오른쪽
-        for (int i = 0; i < width - 1; i++)
-            tv.push_back(board.b[r][c + i]);
-        // 아래쪽
-        for (int i = 0; i < width - 1; i++)
-            tv.push_back(board.b[r + i][c + width - 1]);
-        // 왼쪽
-        for (int i = 0; i < width - 1; i++)
-            tv.push_back(board.b[r + width - 1][c + width - 1 - i]);
-        // 위쪽
-        for (int i = 0; i < width - 1; i++)
-            tv.push_back(board.b[r + width - 1 - i][c]);
+    char c;
+    int t;
+    int s;
+};
 
-        // 2. CW rotate
-        std::rotate(tv.begin(), tv.end() - 1, tv.end());
+vector<Node> nodes;
+vector<int> path, optimalPath;
 
-        // 3. update
-        // i는 매번 초기화 j는 처음만 초기화
-        int i, j;
-        for (i = 0, j = 0; i < width - 1; i++, j++)
-            board.b[r][c + i] = tv[j];
-        // 아래쪽
-        for (i = 0; i < width - 1; i++, j++)
-            board.b[r + i][c + width - 1] = tv[j];
-        // 왼쪽
-        for (i = 0; i < width - 1; i++, j++)
-            board.b[r + width - 1][c + width - 1 - i] = tv[j];
-        // 위쪽
-        for (i = 0; i < width - 1; i++, j++)
-            board.b[r + width - 1 - i][c] = tv[j];
-    }
-}
-
-void dfs(int curr, Board board)
+// 리프까지 거리가 전부 다르다
+void dfs(int curr, int day, int sum, int totalTime)
 {
-    if (curr == K)
+    // 1. 갈 수 있는 노드를 전부 선택
+    // (N개의 브랜치 중에서 호텔/공항은 제외하고 여행지만 탐색)
+    for (int next = 1; next <= N; next++)
     {
-        mn = min(mn, getVal(board));
-        return;
-    }
-
-    for (int i = 0; i < K; i++)
-    {
-        if (!seen[i])
+        if (curr == next)
+            continue;
+        if (nodes[next].c != 'P')
+            continue;
+        if (!seen[next])
         {
-            seen[i] = 1;
-            Board next = board;
-            rotate(next, Ops[i]);
-            dfs(curr + 1, next);
-            seen[i] = 0;
+            int usedTime = totalTime + dist[curr][next] + nodes[next].t;
+            if (usedTime <= TIME_LIMIT)
+            {
+                seen[next] = 1;
+                path.push_back(next);
+                // day는 유지한채로 여행지만 추가 탐색
+                dfs(next, day, sum + nodes[next].s, usedTime);
+                path.pop_back();
+                seen[next] = 0;
+            }
+        }
+    }
+
+    // 2. 도착지 결정 (브랜치를 추가한다)
+    // 2-1. 마지막 날이 아니면 호텔
+    // 호텔에 도착하면 무조건 다음날로 이동한다.
+    if (day < M)
+    {
+        for (auto hotel : hotels)
+        {
+            int usedTime = dist[curr][hotel] + totalTime;
+            if (usedTime <= TIME_LIMIT)
+            {
+                path.push_back(hotel);
+                dfs(hotel, day + 1, sum, 0);
+                path.pop_back();
+            }
+        }
+    }
+    // 2-2. 마지막 날이면 공항
+    else
+    {
+        int usedTime = dist[curr][airport] + totalTime;
+        if (usedTime <= TIME_LIMIT)
+        {
+            if (mx < sum)
+            {
+                mx = sum;
+                optimalPath = path;
+                return;
+            }
         }
     }
 }
+
 int main()
 {
-    int T;
     cin >> T;
     for (int t = 1; t <= T; t++)
     {
-
-        cin >> N >> M >> K;
-        mn = 999;
+        cin >> N >> M;
         for (int i = 1; i <= N; i++)
-            for (int j = 1; j <= M; j++)
-                cin >> board.b[i][j];
+            for (int j = i + 1; j <= N; j++)
+            {
+                cin >> dist[i][j];
+                dist[j][i] = dist[i][j];
+            }
 
-        Ops.assign(K, {});
-        for (int i = 0; i < K; i++)
+        // 초기화
+        seen.assign(N + 1, 0);   // 방문 표식
+        nodes.assign(N + 1, {}); // 노드 전체
+        hotels.clear();
+        mx = 0;
+        path.clear();
+        optimalPath.clear();
+
+        for (int i = 1; i <= N; i++)
         {
-            cin >> Ops[i].r >> Ops[i].c >> Ops[i].s;
+            Node n{};
+            cin >> n.c;
+            if (n.c == 'P')
+            {
+                cin >> n.t >> n.s;
+            }
+            else if (n.c == 'A')
+            {
+                airport = i;
+            }
+            else if (n.c == 'H')
+            {
+                hotels.push_back(i);
+            }
+
+            nodes[i] = n;
         }
 
-        seen.assign(K, 0);
+        dfs(airport, 1, 0, 0);
 
-        dfs(0, board);
-        cout << "#" << t << " " << mn << "\n";
+        cout << "#" << t << " " << mx << " ";
+        for (auto path : optimalPath)
+            cout << path << " ";
+        cout << airport << "\n";
     }
     return 0;
 }
