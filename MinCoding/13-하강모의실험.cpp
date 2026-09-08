104a5701cfcb23c5108e38d4c577c73dc72d52fa
apex737 <apex737@naver.com>
2026-09-08 20:33:19 +0900

    하강모의 init

--- MinCoding/13-하강모의실험.cpp
+++ MinCoding/13-하강모의실험.cpp
@@ -1,13 +1,57 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-
+int N;
+int board[505][505];
+int rotated[505][505];
 int main()
 {
-    int T;
-    cin >> T;
-    for (int t = 1; t <= T; t++)
+  int T;
+  cin >> T;
+  for(int t=1; t<=T; t++)
+  {
+    cin >> N;
+    for(int i=0; i<N; i++)
+      for(int j=0; j<N; j++)
+        cin >> board[i][j];
+    
+    // 1. 하강
+    // 2. 우측하강.. 을 할수도 있지만.. 떨구는 시뮬레이션 함수를
+    // 잘 구현해서 기존 배열을 CW 회전시키면 편할듯
+    int sum = 0;
+    bool mode = false;
+    simulate();
+    
+    for(int i=0; i<N; i++)
+      sum += board[N-1][i];
+    rotateBoard();
+    simulate();
+    for(int i=0; i<N; i++)
+      sum += rotated[N-1][i];
+    
+    cout << "#" << t << " " << sum << "\n";
+
+  }
+  return 0;
+}
+
+void rotateBoard()
+{
+  for(int i=0; i<N; i++)
+    for(int j=0; j<N; j++)
     {
+
     }
-    return 0;
+
+}
+
+void simulate()
+{
+  // 1. 하강
+  for(int j=0; j<N; j++)
+  {
+    // 1-1. 뭐가 있으면 떨군다
+    if(!board[0][j]) continue;
+  }
 }
